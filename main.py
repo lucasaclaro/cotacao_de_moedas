f6945a636b9011858015e0ce23f34dc6b06f6b39
Lucas Claro <102222870+lucasaclaro@users.noreply.github.com>
2022-09-09 11:02:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -33,17 +33,17 @@ img_botao = PhotoImage(file='imagens\\botao.png')
 lab_fundo = Label(master, image=img_fundo)
 lab_fundo.pack()
 
-data_atual = Label(master, text=f'Data:   {data.day} / {data.month} / {data.year}', font=('Open Sans', 11, 'bold'), fg='white', background='#2C735F')
-data_atual.place(width=120, height=34, x=182, y=90)
+data_atual = Label(master, text=f'Data da cotação:   {data.day} / {data.month} / {data.year}', font=('Open Sans', 11, 'bold'), fg="#FEFEFE", bg="#000000")
+data_atual.place(width=200, height=34, x=142, y=10)
 
 
 
 
 botao = Button(master, image=img_botao, command=pegar_cotacoes)
-botao.place(width=167, height=52, x=160, y=272)
+botao.place(width=167, height=52, x=160, y=142)
 
-texto_cotacoes = Label(master, text='', font=('Open Sans', 20, 'bold'), fg='white', background='#2C735F')
-texto_cotacoes.place(width=270, height=158, x=101, y=350)
+texto_cotacoes = Label(master, text='', font=('Open Sans', 20, 'bold'), fg='white', background='#000000')
+texto_cotacoes.place(width=400, height=140, x=31, y=200)
 
 
 
